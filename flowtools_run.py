fb9f8ef1513430cd5a0531ce32a7ef131d37a316
Barnabas Tasi <barnab.tasi03@gmail.com>
2023-01-25 09:36:25 +0100

    .

--- flowtools_run.py
+++ flowtools_run.py
@@ -4,12 +4,21 @@ import matplotlib.pyplot as plt
 
 x_loc = []
 ppt = []
+areas_loc = []
+areas = [] #A/A_t for the fixed nozzle
+
+Mach = []
+TT0 = []
+pp0 = []
+rr0 = []
+aa_ref = []
+
 
 skip = 2
 count = 0
 
+#reading measurements
 f1 = open("testfile.txt", "r")
-
 for x in f1:
     if count >= skip:
         x = x.strip()
@@ -17,21 +26,45 @@ for x in f1:
         x_loc.append(float(values[0]))
         ppt.append(float(values[1].strip()))
     count += 1
-    
+
+#reading fixed nozzle area ratios
+f_area = open("area.txt", "r")
+for line in f_area:
+    areas.append(float(line))
+
+#reading fixed nozzle area ratio locations
+f_arealoc = open("area ratio locations.txt", "r")
+for line in f_arealoc:
+    areas_loc.append(float(line))
+
+#fixed nozzle area ratio arrays
+areas_loc = np.array(areas_loc)
+areas = np.array(areas)
+areas_sub = areas[0:3]
+areas_sup = areas[3:]
+
+#pressure readings with location arrays
 x_loc = np.array(x_loc)
 ppt = np.array(ppt)
 
-plt.plot(x_loc,ppt)
-plt.show()
-
 gamma = 1.4
 
+for i in areas_sub:
+    out = flowtools.flowisentropic2(gamma,i,'sub')
+    Mach.append(out[0])
 
+for i in areas_sup:
+    out = flowtools.flowisentropic2(gamma,i,'sup')
+    Mach.append(out[0])
 
-#out = flowtools.flowisentropic2(gamma,50.0,'sub')
-#print(out)
+print(Mach)
+plt.plot(x_loc,ppt)
+plt.plot(areas_loc,areas)
+plt.plot(areas_loc,Mach)
+plt.show()
 
 #out2 = flowtools.flownormalshock2(gamma,2.5,'mach')
 #print(out2)
 
-f.close()
+f1.close()
+f_area.close()
